e2215d2d7967c4a3251d7626a231fb0f38cb2625
prg0615 <parguzatt@gmail.com>
2022-06-10 20:44:01 +0200

    order food app

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react"
+
+import Button from "./HeaderCartBtn"
+import mealsImage from "../../assets/meals.jpeg"
+import classes from "./Header.module.css"
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <Button />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  )
+}
+
+export default Header
