07a9a762a8d191112ffea79f9a979a9b19b12530
prg0615 <parguzatt@gmail.com>
2022-06-14 11:08:59 +0200

    small react food order app complated

--- src/components/Layout/HeaderCartBtn.js
+++ src/components/Layout/HeaderCartBtn.js
@@ -1,17 +1,42 @@
-import React from "react"
+import { useContext, useEffect, useState } from "react"
 
 import CartIcon from "../Cart/CartIcon"
-
+import CartContext from "../../store/cart-context"
 import classes from "./HeaderCartButton.module.css"
 
 const HeaderCartBtn = (props) => {
+  const [btnIsHighlighted, setBtnHighlighted] = useState(false)
+  const cartCtx = useContext(CartContext)
+  const { items } = cartCtx
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  }, 0)
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnHighlighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   )
 }
